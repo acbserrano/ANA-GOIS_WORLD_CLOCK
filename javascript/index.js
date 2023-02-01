6ae8cd1209c9bbe03cb2a5d8f1a682d52388fcab
acbserrano <121968671+acbserrano@users.noreply.github.com>
2023-02-01 23:13:28 +0000

    city choice working

--- javascript/index.js
+++ javascript/index.js
@@ -17,9 +17,28 @@ function updateTime() {
     let ParisTimeElement = ParisElement.querySelector(".time");
     let ParisTime = moment().tz("Europe/Paris");
 
-  ParisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY");
-  ParisTimeElement.innerHTML = ParisTime.format("h:mm:ss [<small>]A[</small>]");
+    ParisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY");
+    ParisTimeElement.innerHTML = ParisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = `
+<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+ <div class="date">${cityTime.format("MMMM Do YYYY")}</div></div>
+            <div class="time"> ${cityTime.format(
+              "h:mm:ss"
+            )}<small>${cityTime.format(" A")}</small></div>
+        </div></div>`;
 }
 
 updateTime();
